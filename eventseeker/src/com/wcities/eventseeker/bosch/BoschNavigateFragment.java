c0eaf3e4df1a0b1f4165ca46e21d88bc88f8f60c
ankurwcities <ankur@wcities.com>
2014-02-06 18:11:58 +0530

    Extra zooming removed for bosch map.

--- eventseeker/src/com/wcities/eventseeker/bosch/BoschNavigateFragment.java
+++ eventseeker/src/com/wcities/eventseeker/bosch/BoschNavigateFragment.java
@@ -266,7 +266,7 @@ public class BoschNavigateFragment extends FragmentLoadableFromBackStack impleme
 					mySpinMarkerOptions.position(new MySpinLatLng(currentLat, currentLon));
 					mySpinMarkerOptions.icon(bitmapDescriptor);
 					mMap.addMarker(mySpinMarkerOptions);
-			    	mMap.moveCamera(MySpinCameraUpdateFactory.zoomTo(9));
+					mMap.moveCamera(MySpinCameraUpdateFactory.zoomTo(9));
 			    }
 			}
 		}
