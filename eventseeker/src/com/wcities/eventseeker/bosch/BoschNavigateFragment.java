496b9705fe2ffedf76a7358fdf0d366542630ca7
mithilwcities <mithil@wcities.com>
2014-02-06 17:08:48 +0530

    resolved  map conflicts

--- eventseeker/src/com/wcities/eventseeker/bosch/BoschNavigateFragment.java
+++ eventseeker/src/com/wcities/eventseeker/bosch/BoschNavigateFragment.java
@@ -251,8 +251,6 @@ public class BoschNavigateFragment extends FragmentLoadableFromBackStack impleme
 			    	
 			    } else {
 			    	mMap.moveCamera(MySpinCameraUpdateFactory.newLatLng(points.get(0)));
-<<<<<<< HEAD
-<<<<<<< HEAD
 			    	mMap.moveCamera(MySpinCameraUpdateFactory.zoomTo(12));
 			    	
 					MySpinBitmapDescriptor bitmapDescriptor = MySpinBitmapDescriptorFactory.fromResource("ic_des");
@@ -268,12 +266,7 @@ public class BoschNavigateFragment extends FragmentLoadableFromBackStack impleme
 					mySpinMarkerOptions.position(new MySpinLatLng(currentLat, currentLon));
 					mySpinMarkerOptions.icon(bitmapDescriptor);
 					mMap.addMarker(mySpinMarkerOptions);
-=======
 			    	mMap.moveCamera(MySpinCameraUpdateFactory.zoomTo(9));
->>>>>>> bc7e79614441f30f3aa7d9d280efa87a6a3aff6e
-=======
-			    	mMap.moveCamera(MySpinCameraUpdateFactory.zoomTo(9));
->>>>>>> bc7e79614441f30f3aa7d9d280efa87a6a3aff6e
 			    }
 			}
 		}
