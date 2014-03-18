18935a42f0ee3a6fe9f92cbd8f03d5efc871c450
ankurwcities <ankur@wcities.com>
2014-03-18 23:27:23 +0530

    Check for ethernet connectivity added.

--- eventseeker/src/com/wcities/eventseeker/util/NetworkUtil.java
+++ eventseeker/src/com/wcities/eventseeker/util/NetworkUtil.java
@@ -3,6 +3,7 @@ package com.wcities.eventseeker.util;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 
 public class NetworkUtil {
 	  
@@ -13,7 +14,8 @@ public class NetworkUtil {
 		NetworkInfo activeNetwork = cm.getActiveNetworkInfo();	        
 		if (null != activeNetwork) {
 			return (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) 
-	            		|| (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE);
+	            		|| (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) 
+	            		|| (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2 && activeNetwork.getType() == ConnectivityManager.TYPE_ETHERNET);
 		} 
         return false;
 	}
