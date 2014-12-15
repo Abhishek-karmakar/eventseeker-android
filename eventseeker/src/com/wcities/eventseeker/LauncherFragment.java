3483a3d199c7399ca93feb4c2cafc22fad51f11d
mithilwcities <mithil@wcities.com>
2014-12-15 10:56:22 +0530

    added video as background of Launcher screen
    added video as background of Launcher screen

--- eventseeker/src/com/wcities/eventseeker/LauncherFragment.java
+++ eventseeker/src/com/wcities/eventseeker/LauncherFragment.java
@@ -1,6 +1,10 @@
 package com.wcities.eventseeker;
 
 import android.content.Context;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnErrorListener;
+import android.media.MediaPlayer.OnPreparedListener;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,7 +12,11 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -20,11 +28,21 @@ import com.wcities.eventseeker.constants.AppConstants;
 import com.wcities.eventseeker.custom.fragment.FragmentLoadableFromBackStack;
 import com.wcities.eventseeker.util.FragmentUtil;
 
-public class LauncherFragment extends FragmentLoadableFromBackStack implements OnClickListener {
+public class LauncherFragment extends FragmentLoadableFromBackStack implements OnClickListener, Callback {
 
 	private static final String TAG = LauncherFragment.class.getSimpleName();
+
+	private static final int PAGE_DELAY_TIME = 5000;
 	
 	private BannerHandler bannerHandler;
+
+	private SurfaceView srfvVideo;
+	private SurfaceHolder srfcHldr;
+	private MediaPlayer mdPlyr;
+
+	private long[] delay;
+
+	private int videoDuration;
 	
 	private static enum PagerTitle {
 		firstTitle,
@@ -46,6 +64,40 @@ public class LauncherFragment extends FragmentLoadableFromBackStack implements O
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setRetainInstance(true);
+
+		delay = new long[PagerTitle.values().length];
+		for (int i = 0; i < delay.length; i++) {
+			delay[0] = PAGE_DELAY_TIME;
+		}
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_launcher, null);
+		
+		srfvVideo = (SurfaceView) view.findViewById(R.id.srfvVideo);
+		createSurface();
+		
+		view.findViewById(R.id.btnLogin).setOnClickListener(this);
+		view.findViewById(R.id.btnSignUp).setOnClickListener(this);
+
+		ActionBarActivity activity = (ActionBarActivity) FragmentUtil.getActivity(this);
+
+		ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+		viewPager.setAdapter(new TextViewPagerAdapter(activity, getChildFragmentManager()));
+
+		CirclePageIndicator pageIndicator = (CirclePageIndicator) view.findViewById(R.id.pageIndicator);
+		pageIndicator.setViewPager(viewPager);
+		
+        bannerHandler = new BannerHandler(viewPager, delay);
+        pageIndicator.setOnPageChangeListener(bannerHandler);
+		
+		return view;
+	}
+	
+	private void createSurface() {
+		srfcHldr = srfvVideo.getHolder();
+		srfcHldr.addCallback(this);
 	}
 	
 	@Override
@@ -60,27 +112,6 @@ public class LauncherFragment extends FragmentLoadableFromBackStack implements O
 		if (ma.isTabletAndInLandscapeMode()) {
 			ma.hideDrawerList();
 		}
-	}
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_launcher, null);
-		
-		view.findViewById(R.id.btnLogin).setOnClickListener(this);
-		view.findViewById(R.id.btnSignUp).setOnClickListener(this);
-
-		ActionBarActivity activity = (ActionBarActivity) FragmentUtil.getActivity(this);
-
-		ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewPager);
-		viewPager.setAdapter(new TextViewPagerAdapter(activity, getChildFragmentManager()));
-
-		CirclePageIndicator pageIndicator = (CirclePageIndicator) view.findViewById(R.id.pageIndicator);
-		pageIndicator.setViewPager(viewPager);
-		
-        bannerHandler = new BannerHandler(viewPager);
-        pageIndicator.setOnPageChangeListener(bannerHandler);
-		
-		return view;
 	}
 	
 	@Override
@@ -198,5 +229,86 @@ public class LauncherFragment extends FragmentLoadableFromBackStack implements O
 			return view;
 		}
 		
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		playVideo();
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		releaseMediaPlayer();
+	}
+	
+	@Override
+	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {}
+
+	private void playVideo() {
+		try {
+			if (mdPlyr == null) {
+				mdPlyr = MediaPlayer.create(FragmentUtil.getActivity(this).getApplicationContext(), R.raw.event_video);
+			}
+			/**
+			 * the below "mdPlyr != null" check is added as in few phones ex. Samsung Galaxy S, the video wasn't being 
+			 * played and 'mdPlyr' instance at this position was coming as null. This is because in above line where 
+			 * 'mdPlyr' instance is being created 'IOException' occurs and hence the instance remains to be null.
+			 */
+			if (mdPlyr != null) {
+				if (videoDuration > 0) {
+					mdPlyr.seekTo(videoDuration - 1000);
+				}
+				mdPlyr.setScreenOnWhilePlaying(true);				
+				mdPlyr.setDisplay(srfcHldr);
+				mdPlyr.setOnPreparedListener(new OnPreparedListener() {
+					
+					@Override
+					public void onPrepared(MediaPlayer mediaplayer) {
+						int width = mdPlyr.getVideoWidth();
+						int height = mdPlyr.getVideoHeight();
+						
+						if (width != 0 && height != 0) {
+							srfcHldr.setFixedSize(width, height);
+							mdPlyr.start();
+						
+						} else {
+							srfvVideo.setVisibility(View.GONE);						
+						}
+					}
+				});
+				mdPlyr.setOnErrorListener(new OnErrorListener() {
+					
+					@Override
+					public boolean onError(MediaPlayer mp, int what, int extra) {
+						srfvVideo.setVisibility(View.GONE);
+						return false;
+					}
+				});
+				mdPlyr.setOnCompletionListener(new OnCompletionListener() {
+					
+					@Override
+					public void onCompletion(MediaPlayer mp) {
+						mdPlyr.start();
+					}
+				});
+				
+			} else {
+				srfvVideo.setVisibility(View.GONE);	
+			}
+
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+	
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void releaseMediaPlayer() {
+		if (mdPlyr != null) {
+			videoDuration = mdPlyr.getCurrentPosition();
+			mdPlyr.release();
+			mdPlyr = null;
+		}
 	}
 }
