f80483aaabcd5a243b2949b452dcf0bda863c199
mithilwcities <mithil@wcities.com>
2015-05-08 12:07:21 +0530

    Manually copied: PopularArtists FeaturedList changes, bmp load optimization pending
    PopularArtists FeaturedList changes, bmp load optimization pending

--- eventseeker/src/main/java/com/wcities/eventseeker/PopularArtistsFragment.java
+++ eventseeker/src/main/java/com/wcities/eventseeker/PopularArtistsFragment.java
@@ -1,31 +1,81 @@
 package com.wcities.eventseeker;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
+import com.wcities.eventseeker.adapter.RVPopularArtistsAdapter;
+import com.wcities.eventseeker.api.Api;
+import com.wcities.eventseeker.asynctask.LoadPopularArtists;
 import com.wcities.eventseeker.constants.AppConstants;
 import com.wcities.eventseeker.constants.BundleKeys;
 import com.wcities.eventseeker.constants.ScreenNames;
-import com.wcities.eventseeker.core.Artist.Genre;
+import com.wcities.eventseeker.core.Artist;
+import com.wcities.eventseeker.core.FeaturedListArtistCategory;
+import com.wcities.eventseeker.core.PopularArtistCategory;
 import com.wcities.eventseeker.custom.fragment.FragmentLoadableFromBackStack;
+import com.wcities.eventseeker.interfaces.AsyncTaskListener;
+import com.wcities.eventseeker.interfaces.FullScrnProgressListener;
+import com.wcities.eventseeker.interfaces.LoadItemsInBackgroundListener;
+import com.wcities.eventseeker.interfaces.OnPopularArtistsCategoryClickListener;
+import com.wcities.eventseeker.util.AsyncTaskUtil;
+import com.wcities.eventseeker.util.DeviceUtil;
 import com.wcities.eventseeker.util.FragmentUtil;
 
-public class PopularArtistsFragment extends FragmentLoadableFromBackStack implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PopularArtistsFragment extends FragmentLoadableFromBackStack implements FullScrnProgressListener,
+		LoadItemsInBackgroundListener, AsyncTaskListener<Void>, OnPopularArtistsCategoryClickListener {
+
+	private static final String TAG = PopularArtistsFragment.class.getSimpleName();
+
+	private RecyclerView rvPopularArtists;
+	private RelativeLayout rltLytProgressBar;
+
+	private List<PopularArtistCategory> popularArtistCategories;
+	private RVPopularArtistsAdapter popularArtistsAdapter;
+
+	private double[] latlon;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		latlon = DeviceUtil.getLatLon(FragmentUtil.getApplication(this));
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreateView(inflater, container, savedInstanceState);
 		View view = inflater.inflate(R.layout.fragment_popular_artists, null);
 
-		view.findViewById(R.id.btnFeatured).setOnClickListener(this);
-		view.findViewById(R.id.btnMusic).setOnClickListener(this);
-		view.findViewById(R.id.btnComedy).setOnClickListener(this);
-		view.findViewById(R.id.btnTheater).setOnClickListener(this);
-		view.findViewById(R.id.btnSports).setOnClickListener(this);
+		rvPopularArtists = (RecyclerView) view.findViewById(R.id.rvPopularArtists);
+		// use a linear layout manager
+		RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(FragmentUtil.getActivity(this));
+		rvPopularArtists.setLayoutManager(layoutManager);
+
+		rltLytProgressBar = (RelativeLayout) view.findViewById(R.id.rltLytProgressBar);
+		rltLytProgressBar.setBackgroundResource(R.drawable.bg_no_content_overlay);
 
 		return view;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		if (popularArtistCategories == null) {
+			popularArtistCategories = new ArrayList<PopularArtistCategory>();
+			popularArtistCategories.add(null);
+
+			popularArtistsAdapter = new RVPopularArtistsAdapter(popularArtistCategories, this, this, this, this);
+		}
+			rvPopularArtists.setAdapter(popularArtistsAdapter);
 	}
 
 	@Override
@@ -33,42 +83,69 @@ public class PopularArtistsFragment extends FragmentLoadableFromBackStack implem
 		return ScreenNames.POPULAR_ARTISTS_SCREEN;
 	}
 
-	public void onClick(View v) {
+	public void onPopularArtistsCategoryClick(PopularArtistCategory popularArtistCategory) {
 		Bundle args;
-		switch (v.getId()) {
-			case R.id.btnFeatured:
+		switch (popularArtistCategory.getPopularArtistsType()) {
+			case FeaturedListArtists:
+				FeaturedListArtistCategory featuredListArtistCategory = ((FeaturedListArtistCategory) popularArtistCategory);
 				args = new Bundle();
-				args.putSerializable(BundleKeys.GENRE, Genre.Featured);
+				args.putSerializable(BundleKeys.FEATURED_LIST_ARTISTS_ID, featuredListArtistCategory.getId());
+				args.putString(BundleKeys.SCREEN_TITLE, featuredListArtistCategory.getName());
+				((MainActivity) FragmentUtil.getActivity(this))
+					.replaceByFragment(AppConstants.FRAGMENT_TAG_SELECTED_FEATURED_LIST_ARTISTS_FRAGMENT, args);
+				break;
+
+			case FeaturedArtists:
+				args = new Bundle();
+				args.putSerializable(BundleKeys.GENRE, Artist.Genre.Featured);
 				args.putInt(BundleKeys.SCREEN_TITLE, R.string.title_featured_list);
 				((MainActivity) FragmentUtil.getActivity(this))
 						.replaceByFragment(AppConstants.FRAGMENT_TAG_SELECTED_ARTIST_CATEGORY_FRAGMENT, args);
 				break;
 
-			case R.id.btnMusic:
+			case MusicArtists:
 				((MainActivity) FragmentUtil.getActivity(this))
 						.replaceByFragment(AppConstants.FRAGMENT_TAG_MUSIC_ARTISTS, null);
 				break;
 
-			case R.id.btnComedy:
+			case ComedyArtists:
 				args = new Bundle();
-				args.putSerializable(BundleKeys.GENRE, Genre.Comedy);
+				args.putSerializable(BundleKeys.GENRE, Artist.Genre.Comedy);
 				args.putInt(BundleKeys.SCREEN_TITLE, R.string.title_comedy);
 				((MainActivity) FragmentUtil.getActivity(this))
 						.replaceByFragment(AppConstants.FRAGMENT_TAG_SELECTED_ARTIST_CATEGORY_FRAGMENT, args);
 				break;
 
-			case R.id.btnTheater:
+			case TheaterArtists:
 				args = new Bundle();
-				args.putSerializable(BundleKeys.GENRE, Genre.Theater);
+				args.putSerializable(BundleKeys.GENRE, Artist.Genre.Theater);
 				args.putInt(BundleKeys.SCREEN_TITLE, R.string.title_theater);
 				((MainActivity) FragmentUtil.getActivity(this))
 						.replaceByFragment(AppConstants.FRAGMENT_TAG_SELECTED_ARTIST_CATEGORY_FRAGMENT, args);
 				break;
 
-			case R.id.btnSports:
+			case SportsArtists:
 				((MainActivity) FragmentUtil.getActivity(this))
 						.replaceByFragment(AppConstants.FRAGMENT_TAG_SPORTS_ARTISTS, null);
 				break;
 		}
+	}
+
+	@Override
+	public void displayFullScrnProgress() {
+		rltLytProgressBar.setVisibility(View.VISIBLE);
+	}
+
+	@Override
+	public void loadItemsInBackground() {
+		LoadPopularArtists loadPopularArtists = new LoadPopularArtists(Api.OAUTH_TOKEN,
+			popularArtistCategories, latlon[0], latlon[1], popularArtistsAdapter, this);
+		popularArtistsAdapter.setLoadArtists(loadPopularArtists);
+		AsyncTaskUtil.executeAsyncTask(loadPopularArtists, true);
+	}
+
+	@Override
+	public void onTaskCompleted(Void... params) {
+		rltLytProgressBar.setVisibility(View.INVISIBLE);
 	}
 }
--- eventseeker/src/main/java/com/wcities/eventseeker/util/BitmapUtil.java
+++ eventseeker/src/main/java/com/wcities/eventseeker/util/BitmapUtil.java
@@ -1,12 +1,10 @@
 package com.wcities.eventseeker.util;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.wcities.eventseeker.cache.BitmapCacheable;
+import com.wcities.eventseeker.cache.BitmapCacheable.ImgResolution;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -16,33 +14,56 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.BufferedHttpEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-
-import com.wcities.eventseeker.cache.BitmapCacheable;
-import com.wcities.eventseeker.cache.BitmapCacheable.ImgResolution;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BitmapUtil {
-	
-	private static final String TAG = "BitmapUtil";
+
+	private static final String TAG = BitmapUtil.class.getSimpleName();
+	private static final int MAX_BITMAP_DIMENSION = 2048;
 
 	public static Bitmap getBitmap(String url) throws FileNotFoundException {
 		//Log.i(TAG, "getBitmap()");
 		Bitmap bitmap = null;
 		InputStream is = null;
-		
+
 		try {
 			is = (InputStream) new URL(url).getContent();
 			//Log.i(TAG, "got is");
 			bitmap = BitmapFactory.decodeStream(is);
-			//Log.i(TAG, "got bitmap");
-			
+			/**
+			 * NOTE: In few of the devices like Nexus 7, Sometimes an error was showing saying 'bitmap is
+			 * too large to upload' while setting the bitmap on ImageView. So, scaling the bitmaps max dimen
+			 * to be 'MAX_BITMAP_DIMENSION' and keeping the aspect ratio constant, if width or height >
+			 * MAX_BITMAP_DIMENSION.
+			 */
+			int ogWidth = bitmap.getWidth();
+			int ogHeight = bitmap.getHeight();
+			if (ogWidth > MAX_BITMAP_DIMENSION || ogHeight > MAX_BITMAP_DIMENSION) {
+				int scaledWidth, scaledHeight;
+				float aspectRatio = ogWidth / ogHeight;
+				boolean isWidthGreater = (ogWidth > ogHeight);
+				if (isWidthGreater) {
+					scaledWidth = MAX_BITMAP_DIMENSION;
+					scaledHeight = (int) (MAX_BITMAP_DIMENSION / aspectRatio);
+
+				} else {
+					scaledWidth = (int) (MAX_BITMAP_DIMENSION * aspectRatio);
+					scaledHeight = MAX_BITMAP_DIMENSION;
+				}
+					bitmap = Bitmap.createScaledBitmap(bitmap, scaledWidth, scaledHeight, false);
+			}
 			/*URL imageUrl = new URL(url);
-            HttpURLConnection conn = (HttpURLConnection)imageUrl.openConnection();
-            conn.setConnectTimeout(10000);
-            conn.setReadTimeout(10000);
-            is = conn.getInputStream();
-            bitmap = BitmapFactory.decodeStream(is);*/
+			HttpURLConnection conn = (HttpURLConnection)imageUrl.openConnection();
+			conn.setConnectTimeout(10000);
+			conn.setReadTimeout(10000);
+			is = conn.getInputStream();
+			bitmap = BitmapFactory.decodeStream(is);*/
 			
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
@@ -64,6 +85,26 @@ public class BitmapUtil {
 		}
 		//Log.i(TAG, "return bitmap");
 		return bitmap;
+	}
+	public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+		// Raw height and width of image
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+
+		if (height > reqHeight || width > reqWidth) {
+
+			final int halfHeight = height / 2;
+			final int halfWidth = width / 2;
+
+			// Calculate the largest inSampleSize value that is a power of 2 and keeps both
+			// height and width larger than the requested height and width.
+			while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth) {
+				inSampleSize *= 2;
+			}
+		}
+
+		return inSampleSize;
 	}
 
 	public static Bitmap getFBUserBitmap(String strURL) {
